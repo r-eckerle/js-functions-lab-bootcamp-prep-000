c07dce1a6da541801bdff14ce81ed151b389cbb2
Ryan <eckerle_r@protonmail.com>
2018-08-12 02:34:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`;
 }
 
-function holidayCountDown(days, holiday) {
+function holidayCountdown(days, holiday) {
   return `It's ${days} days until ${holiday}!`;
 }
